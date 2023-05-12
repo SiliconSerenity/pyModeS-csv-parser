66d2ea0020d33cf9c0c72db5887c3131bedc4b03
Brian <132161530+SiliconSerenity@users.noreply.github.com>
2023-05-12 17:53:52 -0400

    Improve runner example

--- runner.py
+++ runner.py
@@ -1,14 +1,28 @@
-from watcher import Watcher
+import sys
 import time
+from watcher import Watcher
 
 def data_changed_handler(data):
     print(data)
 
-wd = Watcher('./data')
+if len(sys.argv) < 2:
+    print("Usage: python3 runner.py [dir_path]")
+    sys.exit(1)
+
+dir_path = sys.argv[1]
+wd = Watcher(dir_path)
 wd.subscribe(data_changed_handler)
+
+print("Get initial data:")
+print(wd.getData())
+
+
+
+print("Watching directory: {}".format(dir_path))
+print("Press Ctrl+C to stop.")
 
 try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
-    pass
+    print("Stopping...")
